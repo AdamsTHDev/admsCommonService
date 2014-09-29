42f9b8a188ae88daf3a57c7cb5c5702ea1e0c0f5
kampon8 <kampon8@gmail.com>
2014-09-29 23:56:52 +0700

    Initial

---
+++ src/main/java/com/adms/common/service/App.java
@@ -0,0 +1,13 @@
+package com.adms.common.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

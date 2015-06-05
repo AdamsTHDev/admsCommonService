22c87fa183e62f0d0871c7a470f680da62bd7c34
patawee.cha <patawee.cha@AEGONCLIENT33.AEGONTH.COM>
2015-06-05 10:16:29 +0700

    Update 20150605

--- src/main/java/com/adms/common/service/aop/TimeStampIntercept.java
+++ src/main/java/com/adms/common/service/aop/TimeStampIntercept.java
@@ -24,7 +24,8 @@ public class TimeStampIntercept {
 
 	@Before("execution(* com.adms.bo.*.*.add*(..)) "
 			+ " || execution(* com.adms.*.bo.*.*.add*(..)) "
-			+ " || execution(* com.adms.**.service.**.*Service.add*(..))")
+			+ " || execution(* com.adms.**.service.**.*Service.add*(..)) "
+			+ " || execution(* com.adms.**.service.*Service.add*(..)) ")
 	public void beforeInsert(JoinPoint jp) {
 		Object[] objects = jp.getArgs();
 		int objNum = -1;
